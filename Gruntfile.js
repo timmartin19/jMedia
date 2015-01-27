0a66dcaeffb6aad9c8595c1950193c725a9a947f
timmartin19 <tim.martin@vertical-knowledge.com>
2015-01-26 19:39:42 -0500

    Grunt is working

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+/**
+ * Created by martin on 1/26/15.
+ */
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: [
+                    'src/jquery.media.js',
+                    'src/defaults.js',
+                    'src/players.js',
+                    'src/private.js'
+                ],
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify']);
+
+};
